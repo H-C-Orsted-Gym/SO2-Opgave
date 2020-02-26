02ea373afdfb8b9ffb7dc69dabbda368fe25538c
fredrikj31 <fredrik1206@gmail.com>
2020-02-26 13:58:00 +0100

    Bug fixes, and added date

--- main.py
+++ main.py
@@ -23,71 +23,77 @@
 
 from numpy import random
 import time
+import datetime
 nuværendepH = 7
 nuværendeFritKlor = 1
 nuværendeBundetKlor = 0
 
 def CheckWater(førpH, førFritKlor, førBundetKlor):
-    global nuværendepH
-    nuværendepH = round(random.uniform(førpH - 0.1, førpH + 0.1), 2)
-    print("nuværende pH værdi er:", nuværendepH)
+	global nuværendepH
+	nuværendepH = round(random.uniform(førpH - 0.1, førpH + 0.1), 2)
+	print("Nuværende pH værdi er: " + str(nuværendepH))
 
-    global nuværendeFritKlor
-    nuværendeFritKlor = round(random.uniform(førFritKlor - 0.1, førFritKlor + 0.1), 2)
-    if nuværendeFritKlor <= 0:
-        nuværendeFritKlor = 0
-    print("nuværende frit klor mængde er:", nuværendeFritKlor)
+	global nuværendeFritKlor
+	nuværendeFritKlor = round(random.uniform(førFritKlor - 0.1, førFritKlor + 0.1), 2)
+	if nuværendeFritKlor <= 0:
+		nuværendeFritKlor = 0
+	print("Nuværende frit klor mængde er: " + str(nuværendeFritKlor) + " mg/L")
 
-    
-    global nuværendeBundetKlor
-    nuværendeBundetKlor = round(random.uniform(førBundetKlor - 0.1, førBundetKlor + 0.1), 2)
-    if nuværendeBundetKlor <= 0:
-        nuværendeBundetKlor = 0
-    print("nuværende bundet klor mængde er:", nuværendeBundetKlor)
+	
+	global nuværendeBundetKlor
+	nuværendeBundetKlor = round(random.uniform(førBundetKlor - 0.1, førBundetKlor + 0.1), 2)
+	if nuværendeBundetKlor <= 0:
+		nuværendeBundetKlor = 0
+	print("Nuværende bundet klor mængde er: " + str(nuværendeBundetKlor) + " mg/L")
 
-    
-    
-    
+	
+	
+	
 def CheckData(pH, fritKlor, bundetKlor):
-    
-    if pH >=7.4:
-        while pH >= 7.4:
-            pH -= 0.2
-        global nuværendepH
-        nuværendepH = pH
-        print("vi fjerne noget klor. \n pH'en er nu:", pH)
-    if pH < 7:
-        while pH < 7:
-            pH += 0.2
-        nuværendepH = pH
-        print("vi fjerne noget klor. \n pH'en er nu:", pH)
-        
-    if fritKlor >=3:
-        while fritKlor >= 3:
-            fritKlor -= 0.2
-        global nuværendeFritKlor
-        nuværendeFritKlor = fritKlor
-        print("Vi tilsætter lidt klor.\n fritKlor koncentrationen er nu:", fritKlor)
-    if fritKlor < 0.5:
-        while fritKlor < 0.5:
-            fritKlor += 0.2
-        nuværendeFritKlor = fritKlor
-        print("vi fjerne noget klor. \n fritKlor koncentrationen er nu:", fritKlor)
+	if pH >=7.4:
+		while pH >= 7.4:
+			pH -= 0.2
+		global nuværendepH
+		nuværendepH = pH
+		print("Vi recirkulerer vandet, for at fjerne noget af kloret. \npH'en er nu: " + str(pH))
+	if pH < 7:
+		while pH < 7:
+			pH += 0.2
+		nuværendepH = pH
+		print("Vi recirkulerer vandet, for at fjerne noget af kloret. \npH'en er nu: " + str(pH))
+		
+	if fritKlor >=3:
+		while fritKlor >= 3:
+			fritKlor -= 0.2
+		global nuværendeFritKlor
+		nuværendeFritKlor = fritKlor
+		print("\nVi tilsætter lidt klor. \nfritKlor koncentrationen er nu: " + str(fritKlor) + " mg/L")
+	if fritKlor < 0.5:
+		while fritKlor < 0.5:
+			fritKlor += 0.2
+		nuværendeFritKlor = fritKlor
+		print("\nVi recirkulerer vandet, for at fjerne noget af kloret. \nfritKlor koncentrationen er nu: " + str(fritKlor) + " mg/L")
 
-    if bundetKlor >=1:
-        while bundetKlor >= 1:
-            bundetKlor -= 0.2
-        global nuværendeBundetKlor
-        nuværendeBundetKlor = bundetKlor
-        print(time.asctime( time.localtime(time.time()) ), "Vi filtrere vand ud.\n bundetKlor koncentrationen er nu er nu:", bundetKlor)
+	if bundetKlor >=1:
+		while bundetKlor >= 1:
+			bundetKlor -= 0.2
+		global nuværendeBundetKlor
+		nuværendeBundetKlor = bundetKlor
+		print(time.asctime( time.localtime(time.time()) ), "Vi filtrere vand ud.\n bundetKlor koncentrationen er nu er nu: " + str(bundetKlor) + " mg/L")
 
+def getTime():
+	x = datetime.datetime.now()
+	realTime = x.strftime("%d/%m/%Y - %H:%M:%S")
 
+	return realTime
 
 while True: 
-    time.sleep(5)   
-    CheckWater(nuværendepH,nuværendeFritKlor,nuværendeBundetKlor)
-    CheckData(nuværendepH,nuværendeFritKlor,nuværendeBundetKlor)
-    print("\n\n")
+	time.sleep(5)
+	print("---------------" + getTime() + "---------------")
+	CheckWater(nuværendepH,nuværendeFritKlor,nuværendeBundetKlor)
+	CheckData(nuværendepH,nuværendeFritKlor,nuværendeBundetKlor)
+	print("---------------------------------------------------")
+	print("\n\n")
 
 
 
@@ -105,4 +111,4 @@ while True:
 
 
 
-     
+	 
